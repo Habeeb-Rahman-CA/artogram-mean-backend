3972e301f9b1a8f77091681e9e6df0bea91bdb28
HabeebR07 <habeebrahmanca07@gmail.com>
2024-12-30 20:40:21 +0530

    passed user through response

--- controller/authController.js
+++ controller/authController.js
@@ -31,7 +31,7 @@ const login = async (req, res) => {
             httpOnly: true
         })
 
-        res.status(200).json({ message: 'User logged in', accessToken: token })
+        res.status(200).json({ message: 'User logged in', accessToken: token, user: user })
     } catch (err) {
         res.status(500).json({ message: 'Login failed' })
     }
