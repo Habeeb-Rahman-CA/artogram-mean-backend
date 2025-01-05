04f6ab4e26eab3de43314bb2c2c139c81e3d0bd0
HabeebR07 <habeebrahmanca07@gmail.com>
2025-01-05 21:59:26 +0530

    added new apis based on adddress

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel')
 const addAddress = async (req, res) => {
     try {
         const { fullName, phoneNumber, address, street, landmark, city, pincode, state } = req.body
-        const user = await User.findById(req.body.id)
+        const user = await User.findById(req.user.id)
         user.addresses.push({ fullName, phoneNumber, address, street, landmark, city, pincode, state })
         await user.save()
         res.status(200).json({ message: 'Added new address', user: user.addresses })
@@ -12,13 +12,28 @@ const addAddress = async (req, res) => {
     }
 }
 
-const getAddress = async(req, res) =>{
+const getAddress = async (req, res) => {
     try {
-        const user = await user.findById(req.user.id)
-        res.status(200).json({message: 'fetched all the data', addresses: user.addresses})
-        } catch (err) {
-        res.status(500).json({message: 'failed to fetch', err: err.message})
+        const user = await User.findById(req.user.id)
+        res.status(200).json({ message: 'fetched all the data', addresses: user.addresses })
+    } catch (err) {
+        res.status(500).json({ message: 'failed to fetch', err: err.message })
     }
 }
 
-module.exports = { addAddress, getAddress }
+const deleteAddress = async (req, res) => {
+    console.log('hey');
+    try {
+        const userId = req.user.id
+        const addressId  = req.params.id
+        const user = await User.findByIdAndUpdate(userId,
+            { $pull: { addresses: { _id: addressId } } },
+            { new: true }
+        )
+        res.status(200).json({message: "deleted successfully", user})
+    } catch (err) {
+        res.status(500).json({ message: 'failed to delete', err: err.message })
+    }
+}
+
+module.exports = { addAddress, getAddress, deleteAddress }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { addAddress, getAddress } = require('../controller/userController')
+const { addAddress, getAddress, deleteAddress } = require('../controller/userController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/address').post(protect, addAddress).get(protect, getAddress)
+router.route('/address/:id').delete(protect, deleteAddress)
 
 module.exports = router
